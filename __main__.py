2fecc4f9c5c16f6ca430e6cbd5c1743c1ba3469c
Alfredo Palhares <alfredo@palhares.me>
2024-02-26 09:38:18 +0000

    Add Digital Ocean K8s cluster

---
+++ __main__.py
@@ -0,0 +1,48 @@
+"""A DigitalOcean Python Pulumi program"""
+
+import pulumi
+import pulumi_digitalocean as do
+import os
+import lib.utils as utils
+
+
+# Vesions and other global options
+k8s_options = {
+    "do": "1.29.1-do.0",
+    "k8s": "1.29.1"
+}
+
+
+if "DIGITALOCEAN_TOKEN" not in os.environ:
+    raise Exception("Missing DIGITALOCEAN_TOKEN")
+
+do_k8s_versions = utils.validate_k8s_versions(os.environ.get("DIGITALOCEAN_TOKEN"), k8s_options["k8s"])
+
+pulumi.log.info("Available k8s versions: {v}".format(v=do_k8s_versions))
+
+if k8s_options["do"] not in do_k8s_versions:
+    raise Exception("The version that is available in DO is not the same")
+
+# Create a digital ocean kubernetes cluster
+do_k8s_cluster = do.KubernetesCluster("do-k8s-cluster",
+    region="ams3",
+    version=k8s_options["do"],
+    node_pool={
+        "name": "default-pool",
+        "size": "s-2vcpu-2gb",
+        "node_count": 3,
+    },
+    tags=["pulumi", "k8s-cluster"]
+)
+
+# Export the cluster's kubeconfig
+pulumi.export('kubeconfig',
+    do_k8s_cluster.kube_configs.apply(lambda configs: configs[0]['raw_config']))
+
+# Collect all in a project
+prod_project = do.Project(resource_name="production",
+                          description="Production environment", 
+                          environment="Production",
+                          purpose="Production K8S Cluster", 
+                          is_default=False,
+                          resources=[do_k8s_cluster.cluster_urn])
