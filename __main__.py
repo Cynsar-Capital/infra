281bc1752c637e61174052bc101b26e546b48cee
Alfredo Palhares <alfredo@palhares.me>
2024-02-27 19:10:42 +0000

    Create Database

--- __main__.py
+++ __main__.py
@@ -36,13 +36,14 @@ if k8s_options["do"] not in do_k8s_versions:
 
 # Create a digital ocean kubernetes cluster
 do_k8s_cluster = do.KubernetesCluster("do-k8s-cluster",
-    region="ams3",
+    region=default_region,
     version=k8s_options["do"],
     node_pool={
         "name": "default-pool",
         "size": "s-2vcpu-2gb",
         "node_count": 3,
     },
+    vpc_uuid=ams_vpc.id,
     tags=["pulumi", "k8s-cluster"]
 )
 
@@ -50,10 +51,22 @@ do_k8s_cluster = do.KubernetesCluster("do-k8s-cluster",
 pulumi.export('kubeconfig',
     do_k8s_cluster.kube_configs.apply(lambda configs: configs[0]['raw_config']))
 
+do_mysql_db = do.DatabaseCluster("mysql-personal", 
+    engine = "mysql", 
+    node_count=1,
+    version="8",
+    region = default_region,
+    size="db-s-1vcpu-1gb",
+    private_network_uuid=ams_vpc.id,
+    opts = pulumi.ResourceOptions(protect=True),
+    tags=["pulumi", "protected"])
+
 # Collect all in a project
 prod_project = do.Project(resource_name="production",
-                          description="Production environment", 
-                          environment="Production",
-                          purpose="Production K8S Cluster", 
-                          is_default=False,
-                          resources=[do_k8s_cluster.cluster_urn])
+    description="Production environment", 
+    environment="Production",
+    purpose="Production K8S Cluster", 
+    is_default=False,
+    resources=[do_k8s_cluster.cluster_urn,
+               do_mysql_db.cluster_urn])
+
