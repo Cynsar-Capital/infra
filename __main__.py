93b27bae016fc3414f2a8b461a1db9ebe2a937b2
Alfredo Palhares <alfredo@palhares.me>
2024-02-27 19:10:19 +0000

    Import AMS3 VPC

--- __main__.py
+++ __main__.py
@@ -7,15 +7,26 @@ import lib.utils as utils
 
 
 # Vesions and other global options
+default_region = "ams3" # All the infra is on amsterdan
 k8s_options = {
     "do": "1.29.1-do.0",
     "k8s": "1.29.1"
 }
 
-
 if "DIGITALOCEAN_TOKEN" not in os.environ:
     raise Exception("Missing DIGITALOCEAN_TOKEN")
 
+# Main VPC
+ams_vpc = do.Vpc("ams3-vpc",
+                 region=default_region, 
+                 ip_range="10.110.0.0/20",
+                 name="default-ams3",
+                 opts=pulumi.ResourceOptions(
+                    import_="e955595a-be66-4f4b-83d5-7c6a75ba2dcf",
+                    protect=True
+                ))
+
+# K8s Cluster
 do_k8s_versions = utils.validate_k8s_versions(os.environ.get("DIGITALOCEAN_TOKEN"), k8s_options["k8s"])
 
 pulumi.log.info("Available k8s versions: {v}".format(v=do_k8s_versions))
